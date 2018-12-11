fe9001e0b6fa89efedb2b108f07376bed5c89168
MikeK <mgkittridge@gmail.com>
2018-12-11 16:17:41 +1300

    update

--- app.py
+++ app.py
@@ -11,12 +11,52 @@ from pdsql import mssql
 from pyproj import Proj, transform
 import urllib
 
+pd.options.display.max_columns = 10
+
+
 app = dash.Dash(__name__)
 server = app.server
 
 #######################################
 ### Functions
 
+
+def rd_ts_summ(server, db, ts_summ_table, dataset_table, mtype_table, sites_table, sites_cols):
+    ## Load ts summary data
+    ts_summ1 = mssql.rd_sql(server, db, ts_summ_table)
+    ts_summ1['FromDate'] = pd.to_datetime(ts_summ1['FromDate'])
+    ts_summ1['ToDate'] = pd.to_datetime(ts_summ1['ToDate'])
+
+    ## Load other data
+    datasets1 = mssql.rd_sql(server, db, dataset_table)
+    mtypes = mssql.rd_sql(server, db, mtype_table)
+    datasets = pd.merge(datasets1, mtypes, on='MeasurementType')
+    datasets['Dataset Name'] = datasets.Feature + ' - ' + datasets.MeasurementType + ' - ' + datasets.CollectionType + ' - ' + datasets.DataCode + ' - ' + datasets.DataProvider + ' (' + datasets.Units + ')'
+
+    ## Merge the two
+    ts_summ2 = pd.merge(datasets, ts_summ1, on='DatasetTypeID')
+
+    ## Get site info
+    sites = mssql.rd_sql(server, db, sites_table, sites_cols)
+    sites['NZTMX'] = sites['NZTMX'].astype(int)
+    sites['NZTMY'] = sites['NZTMY'].astype(int)
+
+    # Hover text
+    sites.loc[sites.ExtSiteName.isnull(), 'ExtSiteName'] = ''
+
+    sites['hover'] = sites.ExtSiteID + '<br>' + sites.ExtSiteName.str.strip()
+
+    # Convert projections
+    xy1 = list(zip(sites['NZTMX'], sites['NZTMY']))
+    x, y = list(zip(*[transform(from_crs1, to_crs1, x, y) for x, y in xy1]))
+
+    sites['lon'] = x
+    sites['lat'] = y
+
+    ## Combine with everything
+    ts_summ = pd.merge(sites, ts_summ2, on='ExtSiteID')
+
+    return ts_summ
 
 def sel_ts_summ(ts_summ, features, mtypes, ctypes, data_codes, data_providers, start_date, end_date):
     if isinstance(features, str):
@@ -35,9 +75,9 @@ def sel_ts_summ(ts_summ, features, mtypes, ctypes, data_codes, data_providers, s
 
     df['FromDate'] = df['FromDate'].dt.date.astype(str)
     df['ToDate'] = df['ToDate'].dt.date.astype(str)
-    df['Min'] = df['Min'].round(3)
-    df['Mean'] = df['Mean'].round(3)
-    df['Max'] = df['Max'].round(3)
+    df['Min'] = df['Min'].round(3).astype(str)
+    df['Mean'] = df['Mean'].round(3).astype(str)
+    df['Max'] = df['Max'].round(3).astype(str)
     return df
 
 
@@ -51,73 +91,24 @@ ts_summ_table = 'TSDataNumericDailySumm'
 ts_table = 'TSDataNumericDaily'
 dataset_table = 'vDatasetTypeNamesActive'
 mtype_table = 'MeasurementType'
+sites_cols = ['ExtSiteID', 'ExtSiteName', 'NZTMX', 'NZTMY']
 #datasettypes = [4, 5, 15]
 #datasettype_names = {4: 'Water Level (m)', 5: 'Flow (m3/s)', 15: 'Precip (mm)'}
-
-sites = mssql.rd_sql(server, db, sites_table)
-ts_summ1 = mssql.rd_sql(server, db, ts_summ_table)
-
-ts_summ1['FromDate'] = pd.to_datetime(ts_summ1['FromDate'])
-ts_summ1['ToDate'] = pd.to_datetime(ts_summ1['ToDate'])
-
-datasets1 = mssql.rd_sql(server, db, dataset_table)
-mtypes = mssql.rd_sql(server, db, mtype_table)
-datasets = pd.merge(datasets1, mtypes, on='MeasurementType')
-datasets['Dataset Name'] = datasets.Feature + ' - ' + datasets.MeasurementType + ' - ' + datasets.CollectionType + ' - ' + datasets.DataCode + ' - ' + datasets.DataProvider + ' (' + datasets.Units + ')'
-
-ts_summ = pd.merge(datasets, ts_summ1, on='DatasetTypeID')
 
 ts_plot_height = 600
 map_height = 700
 
-sites = sites[sites.ExtSiteID.isin(ts_summ.ExtSiteID.unique())].copy()
-
 ### Convert projections
 from_crs1 = Proj('+proj=tmerc +ellps=GRS80 +a=6378137.0 +f=298.257222101 +towgs84=0,0,0,0,0,0,0 +pm=0  +lon_0=173 +x_0=1600000 +y_0=10000000 +k_0=0.9996 +lat_0=0 +units=m +axis=enu +no_defs', preserve_units=True)
 to_crs1 = Proj('+proj=longlat +datum=WGS84 +ellps=WGS84 +a=6378137 +f=298.257223563 +pm=0  +no_defs', preserve_units=True)
-xy1 = list(zip(sites['NZTMX'], sites['NZTMY']))
-x, y = list(zip(*[transform(from_crs1, to_crs1, x, y) for x, y in xy1]))
-
-sites['lon'] = x
-sites['lat'] = y
 
 init_dataset = 'River - Flow - Recorder - Primary - ECan (m**3/s)'
 
-table_cols = ['ExtSiteID', 'DatasetTypeID', 'Feature', 'MeasurementType', 'CollectionType', 'DataCode', 'DataProvider', 'Units', 'Min', 'Mean', 'Max', 'Count', 'FromDate', 'ToDate']
-
-### Make hover text
-
-#dict1 = {}
-#
-#for group, val in ts_summ.groupby('ExtSiteID'):
-#    hov1 = [group]
-#    for name, t in val.iterrows():
-#        hov1.append('{} from {} to {}'.format(t['Dataset Name'], str(t['FromDate'].date()), str(t['ToDate'].date())))
-#    hov2 = '<br>'.join(hov1)
-#    dict1.update({group: hov2})
-#
-#names1 = pd.DataFrame.from_dict(dict1, orient='index', columns=['hover'])
-#names1.index.name = 'ExtSiteID'
-#
-#sites = pd.merge(sites, names1.reset_index(), on='ExtSiteID')
-
-
-
-sites.loc[sites.ExtSiteName.isnull(), 'ExtSiteName'] = ''
-
-sites['hover'] = sites.ExtSiteID + '<br>' + sites.ExtSiteName.str.strip()
+table_cols = ['ExtSiteID', 'ExtSiteName', 'NZTMX', 'NZTMY', 'DatasetTypeID', 'Feature', 'MeasurementType', 'CollectionType', 'DataCode', 'DataProvider', 'Units', 'Min', 'Mean', 'Max', 'Count', 'FromDate', 'ToDate']
 
 lat = -43.45
 lon = 171.9
-
-### prepare summaries and initial states
-min_year = ts_summ.FromDate.min().year
-max_year = ts_summ.ToDate.max().year
-max_date = ts_summ.ToDate.max()
-
-start_date = max_date - pd.DateOffset(years=1)
-
-init_summ = sel_ts_summ(ts_summ, 'River', 'Flow', 'Recorder', 'Primary', 'ECan', str(start_date.date()), str(max_date.date()))
+zoom = 7
 
 #years = np.arange(min_year2 + 2, max_year + 1)
 #years_label = {str(min_year2): 'All Years'}
@@ -135,11 +126,19 @@ mapbox_access_token = "pk.eyJ1IjoibXVsbGVua2FtcDEiLCJhIjoiY2pudXE0bXlmMDc3cTNxbn
 ###############################################
 ### App layout
 
-map_layout = dict(mapbox = dict(layers = [], accesstoken = mapbox_access_token, style = 'outdoors', center=dict(lat=-43.45, lon=171.9), zoom=7), margin = dict(r=0, l=0, t=0, b=0), autosize=True, hovermode='closest', height=600)
+map_layout = dict(mapbox = dict(layers = [], accesstoken = mapbox_access_token, style = 'outdoors', center=dict(lat=lat, lon=lon), zoom=zoom), margin = dict(r=0, l=0, t=0, b=0), autosize=True, hovermode='closest', height=map_height)
 
+def serve_layout():
 
-app.layout = html.Div(children=[
+    ts_summ = rd_ts_summ(server, db, ts_summ_table, dataset_table, mtype_table, sites_table, sites_cols)
 
+    ### prepare summaries and initial states
+    max_date = pd.Timestamp.now()
+    start_date = max_date - pd.DateOffset(years=1)
+
+    init_summ = sel_ts_summ(ts_summ, 'River', 'Flow', 'Recorder', 'Primary', 'ECan', str(start_date.date()), str(max_date.date()))
+
+    layout = html.Div(children=[
     html.Div([
         html.P(children='Filter sites by:'),
 		html.Label('Feature'),
@@ -154,9 +153,9 @@ app.layout = html.Div(children=[
 		dcc.Dropdown(options=[{'label': d, 'value': d} for d in np.sort(ts_summ.DataProvider.unique())], multi=True, value='ECan', id='data_providers'),
         html.Label('Date Range'),
 		dcc.DatePickerRange(
-            end_date=max_date,
+            end_date=str(max_date.date()),
             display_format='DD/MM/YYYY',
-            start_date=start_date,
+            start_date=str(start_date.date()),
             id='date_sel'
 #               start_date_placeholder_text='DD/MM/YYYY'
             ),
@@ -165,6 +164,7 @@ app.layout = html.Div(children=[
 		], className='two columns', style={'margin': 20}),
 
 	html.Div([
+        html.P('Click on a site or "box select" multiple sites:', style={'display': 'inline-block'}),
 		dcc.Graph(id = 'site-map', style={'height': map_height}),
 
         html.A(
@@ -180,7 +180,11 @@ app.layout = html.Div(children=[
             columns=[{"name": i, "id": i, 'deletable': True} for i in table_cols],
             data=init_summ[table_cols].astype(str).to_dict('rows'),
             sorting=True,
-            sorting_type="multi"
+            sorting_type="multi",
+            style_cell={
+                'minWidth': '80px', 'maxWidth': '200px',
+                'whiteSpace': 'normal'
+            },
 #            column_widths=[20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20]
             )
 
@@ -188,7 +192,7 @@ app.layout = html.Div(children=[
 
     html.Div([
 
-		html.P('Select Dataset:', style={'display': 'inline-block'}),
+		html.P('Select Dataset for time series plot:', style={'display': 'inline-block'}),
 		dcc.Dropdown(options=[{'value:': 5, 'label': init_dataset}], value=5, id='sel_dataset'),
 		dcc.Graph(
 			id = 'selected-data',
@@ -210,17 +214,27 @@ app.layout = html.Div(children=[
             target="_blank",
             style={'margin': 50})
 	], className='six columns', style={'margin': 10, 'height': 900}),
-    html.Div(id='summ_data', style={'display': 'none'})
+    html.Div(id='summ_data', style={'display': 'none'}),
+    html.Div(id='summ_data_all', style={'display': 'none'}, children=ts_summ.to_json(date_format='iso', orient='split'))
 ], style={'margin':0})
+
+    return layout
+
+
+app.layout = serve_layout
 
 app.css.append_css({'external_url': 'https://codepen.io/plotly/pen/EQZeaW.css'})
 
 ########################################
 ### Callbacks
 
+
 @app.callback(
-    Output('summ_data', 'children'), [Input('features', 'value'), Input('mtypes', 'value'), Input('ctypes', 'value'), Input('data_codes', 'value'), Input('data_providers', 'value'), Input('date_sel', 'start_date'), Input('date_sel', 'end_date')])
-def calc_summ(features, mtypes, ctypes, data_codes, data_providers, start_date, end_date):
+    Output('summ_data', 'children'), [Input('features', 'value'), Input('mtypes', 'value'), Input('ctypes', 'value'), Input('data_codes', 'value'), Input('data_providers', 'value'), Input('date_sel', 'start_date'), Input('date_sel', 'end_date')], [State('summ_data_all', 'children')])
+def calc_summ(features, mtypes, ctypes, data_codes, data_providers, start_date, end_date, summ_data_all):
+    ts_summ = pd.read_json(summ_data_all, orient='split')
+    ts_summ['FromDate'] = pd.to_datetime(ts_summ['FromDate'])
+    ts_summ['ToDate'] = pd.to_datetime(ts_summ['ToDate'])
     new_summ = sel_ts_summ(ts_summ, features, mtypes, ctypes, data_codes, data_providers, start_date, end_date).copy()
     print(features, mtypes, ctypes, data_codes, data_providers, start_date, end_date)
     return new_summ.to_json(date_format='iso', orient='split')
@@ -232,7 +246,7 @@ def calc_summ(features, mtypes, ctypes, data_codes, data_providers, start_date, 
 		[State('site-map', 'relayoutData')])
 def display_map(summ_data, relay):
     new_summ = pd.read_json(summ_data, orient='split')
-    new_sites = sites[sites.ExtSiteID.isin(new_summ.ExtSiteID.astype(str).unique())].copy()
+    new_sites = new_summ.drop_duplicates('ExtSiteID')
 #    print(new_sites)
 #    print(new_summ.ExtSiteID.unique())
 
@@ -340,6 +354,7 @@ def display_data(sites, sel_dataset, selected, clicked, start_date, end_date):
     [Input('summ_data', 'children'), Input('sites', 'value'), Input('site-map', 'selectedData'), Input('site-map', 'clickData')])
 def plot_table(summ_data, sites, selectedData, clickData):
     new_summ = pd.read_json(summ_data, orient='split')[table_cols]
+
     if sites:
         new_summ = new_summ.loc[new_summ.ExtSiteID.isin(sites)]
     return new_summ.to_dict("rows")
